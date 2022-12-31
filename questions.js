591aee771c9daf376d178ff6fcfa3ad502b3968c
codingshark200 <d.busch.coding@gmail.com>
2022-12-31 02:33:56 +0100

    Testfragen in questions erstellt und ueberschrift

---
+++ questions.js
@@ -0,0 +1,22 @@
+questions = [
+
+    {
+        "question" : "Wer war Luke Skywalkers Vater?",
+        "answers" : ["Darth Maul", "Darth Sidious", "Count Doku", "General Grevious"],
+        "correctAnswer" : "General Grevious",
+        "imageURI" : "" 
+    },
+    {
+        "question" : "In welcher Serie geht es nicht um die U.S.S Enterprise?",
+        "answers" : ["Star Trek - Enterprise", "Star Trek", "Star Trek - Raumschiff Voyager", "Star Trek - Die nächste Generation"],
+        "correctAnswer" : "Star Trek - Raumschiff Voyager",
+        "imageURI" : "" 
+    },
+    {
+        "question" : "Wer spielte den Boxer 'Rocky Balboa'?",
+        "answers" : ["Arnold Schwarzenegger", "Silvester Stallone", "Bruce Willis", "Channing Tatum"],
+        "correctAnswer" : "Silvester Stallone",
+        "imageURI" : "" 
+    }
+
+]
